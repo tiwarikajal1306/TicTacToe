64fbb655c28abc8e6c659f296136388baaacfd00
kajal <tiwarikajal1306@gmail.com>
2020-06-10 21:47:08 +0530

    Resetting the board

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,20 @@
-#!/bin/bash -x
 echo "Welcome To TicTacToe"
+NUM_OF_CELLS=9
+#variables
+declare -a board
+
+function resetBoard()
+{
+   for ((i=1; i<=NUM_OF_CELLS; i++))
+   do
+         board[i]=$i
+   done
+echo "Board is display : "
+printf "|| ${board[1]} || ${board[2]} || ${board[3]} ||\n"
+printf "|| ${board[4]} || ${board[5]} || ${board[6]} ||\n"
+printf "|| ${board[7]} || ${board[8]} || ${board[9]} ||\n"
+
+}
+
+resetBoard
+
